d1c417ca2614cabb4e72e5b612e8bd1757b8a481
pinhobrunodev <iaishowcs@gmail.com>
2022-01-19 16:37:51 -0300

    Custom Listener ( Same topic, same containerFactory != groupId)

--- consumer/consumer/src/main/java/com/pinhobrunodev/consumer/listener/TestListener.java
+++ consumer/consumer/src/main/java/com/pinhobrunodev/consumer/listener/TestListener.java
@@ -1,16 +1,10 @@
 package com.pinhobrunodev.consumer.listener;
 
+import com.pinhobrunodev.consumer.custom.PersonCustomListener;
 import com.pinhobrunodev.consumer.model.Person;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.util.TimeZone;
 
 @Log4j2
 @Component
@@ -23,8 +17,19 @@ public class TestListener {
 
 
     // containerFactory -> Especificando como vai tratas as mensagens vindo do topico (deserialization)
-    @KafkaListener(topics = "person-topic", groupId = "group-1",containerFactory = "personConcurrentKafkaListenerContainerFactory")
-    public void listenPerson(Person person) {
-        log.info("Pessoa: {} ",person);
+    // @KafkaListener(topics = "person-topic", groupId = "group-1",containerFactory = "personConcurrentKafkaListenerContainerFactory")
+    @PersonCustomListener(groupId = "group-1")
+    public void create(Person person) {
+        log.info("Thread: {}", Thread.currentThread().getId()); // Cada listener abre uma Thread
+        log.info("Criar: {} ", person);
+    }
+
+    // Ambos vao estar escutando o mesmo topico e realizando a mesma deserializaçao de Pessoa porem com group-id diferente
+
+    // @KafkaListener(topics = "person-topic", groupId = "group-2",containerFactory = "personConcurrentKafkaListenerContainerFactory")
+    @PersonCustomListener(groupId = "group-2")
+    public void history(Person person) {
+        log.info("Thread: {}", Thread.currentThread().getId());
+        log.info("Histórico: {} ", person);
     }
 }
