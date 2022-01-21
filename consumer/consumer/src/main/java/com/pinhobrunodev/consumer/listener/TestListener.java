e4daf845a1763c986c217cecb8cad40b05e1c150
pinhobrunodev <iaishowcs@gmail.com>
2022-01-21 14:26:58 -0300

    Headers em lote

--- consumer/consumer/src/main/java/com/pinhobrunodev/consumer/listener/TestListener.java
+++ consumer/consumer/src/main/java/com/pinhobrunodev/consumer/listener/TestListener.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
@@ -47,11 +48,20 @@ public class TestListener {
         log.info("Criar Pessoa: {}", person);
     }
 
-    @KafkaListener(topics = "city-topic", groupId = "group-2", containerFactory = "jsonKafkaListenerContainerFactory")
-    public void createCity(List<City> cities) {
+ @KafkaListener(topics = "city-topic", groupId = "group-2", containerFactory = "jsonKafkaListenerContainerFactory")
+    public void createCity(List<City> cities,@Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Long> partitions) {
         log.info("Cidades: {}", cities);
+        log.info("Partitions: {}", partitions);
     }
 
+
+    /*@KafkaListener(topics = "city-topic", groupId = "group-2", containerFactory = "jsonKafkaListenerContainerFactory")
+    public void createCity(List<Message<City>> messages) {
+        log.info("Messages: {}", messages);
+        var city = messages.get(0).getPayload();
+        log.info("City: {}", city);
+        log.info("Headers : {}",messages.get(0).getHeaders());
+    }*/
     /*@PersonCustomListener(groupId = "group-2")
     public void history(Person person) {
         log.info("Thread: {}", Thread.currentThread().getId());
